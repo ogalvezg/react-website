661b4da35b0d20e8bc5233949c61d2218d2e80b7
Omar Galvez <itsomargalvez@gmail.com>
2020-10-14 15:46:47 +0200

    Hero section complete

---
+++ src/components/Cards.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Cards = () => {
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations</h1>
+            
+        </div>
+    )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from './Button';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
@@ -18,13 +18,17 @@ export default function Navbar() {
         }
     };
 
+    useEffect(() => {
+        showButton();
+    }, []);
+
     window.addEventListener('resize', showButton);
 
     return (
         <>
            <nav className="navbar">    
                 <div className="navbar-container">
-                    <Link to="/" className="navbar-logo" >
+                    <Link to="/" className="navbar-logo" onClick= {closeMobileMenu} >
                         TRVL <i className="fab fa-typo3"></i>
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
