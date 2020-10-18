f78879535b90cd72b4e944c222077f5dc1cf3528
Omar Galvez <itsomargalvez@gmail.com>
2020-10-18 23:18:36 +0200

    first row of cards add

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import CardItem from './CardItem'
+import './Cards.css';
 
 export const Cards = () => {
     return (
         <div className='cards'>
-            <h1>Check out these EPIC Destinations</h1>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__item">
+                        <CardItem 
+                            src='images/img-9.jpg'
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-2.jpg'
+                            text='Travel through the Islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                    </ul>
+                </div>
+            </div>
             
         </div>
-    )
+    );
 }
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../../App.css';
+import { Cards } from '../Cards';
 import HeroSection from '../HeroSection';
 
 export default function Home() {
     return (
         <>
             <HeroSection />
+            <Cards />
         </>
     );
 }
