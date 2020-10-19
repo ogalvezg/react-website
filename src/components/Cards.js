ffbeb747372800da13b3ac5fc3f08d075f93ea26
Omar Galvez <itsomargalvez@gmail.com>
2020-10-19 19:34:27 +0200

    footer complete

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import CardItem from './CardItem'
 import './Cards.css';
+import CardItem from './CardItem'
+
 
 export const Cards = () => {
     return (
@@ -8,7 +9,7 @@ export const Cards = () => {
             <h1>Check out these EPIC Destinations!</h1>
             <div className="cards__container">
                 <div className="cards__wrapper">
-                    <ul className="cards__item">
+                    <ul className="cards__items">
                         <CardItem 
                             src='images/img-9.jpg'
                             text='Explore the hidden waterfall deep inside the Amazon Jungle'
@@ -20,6 +21,26 @@ export const Cards = () => {
                             text='Travel through the Islands of Bali in a Private Cruise'
                             label='Luxury'
                             path='/services'
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                            src='images/img-3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/services'
+                            />
+                            <CardItem
+                            src='images/img-4.jpg'
+                            text='Experience Football on Top of the Himilayan Mountains'
+                            label='Adventure'
+                            path='/products'
+                            />
+                            <CardItem
+                            src='images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adrenaline'
+                            path='/sign-up'
                         />
                     </ul>
                 </div>
